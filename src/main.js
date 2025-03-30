b57b2ed887fec765debead15c966f48dadf73086
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-03-30 22:13:46 +0200

    v.1.15. Added styles ellipses-light.css and review-card.js + review-swiper.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import './js/hero-parallax.js';
 import './js/mobile-menu.js';
 import './js/services-flip-card.js';
 import './js/modal-rent.js';
-
+import './js/review-card.js';
+import './js/review-swiper.js';
