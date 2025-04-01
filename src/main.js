b32e0e3f2538613d3e7a5661d7aa77b4dd9363a4
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-01 22:29:51 +0200

    v.1.17. Added yacht-card.js + yacht-swiper.js + fixed bugs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './js/hero-parallax.js';
 import './js/mobile-menu.js';
+import './js/yacht-card.js';
+import './js/yacht-swiper.js';
 import './js/services-flip-card.js';
 import './js/modal-rent.js';
 import './js/review-card.js';
