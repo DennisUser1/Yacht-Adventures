36bdd4b23ced74d930307cd7bc821658cd7a5495
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-02 19:58:15 +0200

    Initial empty gh-pages branch

---
+++ src/js/review-card.js
@@ -0,0 +1,6 @@
+VanillaTilt.init(document.querySelectorAll('.reviews-item'), {
+    max: 0,
+    speed: 400,
+    glare: true,
+    'max-glare': 0.2,
+});
---
+++ src/js/yacht-swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: getSlidesPerView(),
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+      swiper.params.slidesPerView = getSlidesPerView();
+      swiper.update();
+    },
+  },
+});
+
+function getDirection() {
+  const windowWidth = window.innerWidth;
+  return windowWidth <= 115 ? 'vertical' : 'horizontal';
+}
+
+function getSlidesPerView() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth >= 1280) {
+    return 3;
+  } else if (windowWidth >= 768) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
