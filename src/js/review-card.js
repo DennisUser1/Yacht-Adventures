d218c6c3a200f6d05d192bfa2648b7b73515c953
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-02 20:18:50 +0200

    Updates

--- src/js/review-card.js
+++
@@ -1,6 +0,0 @@
-VanillaTilt.init(document.querySelectorAll('.reviews-item'), {
-    max: 0,
-    speed: 400,
-    glare: true,
-    'max-glare': 0.2,
-});
--- src/js/yacht-card.js
+++
@@ -1,6 +0,0 @@
-VanillaTilt.init(document.querySelectorAll('.yachts-block__card-set'), {
-    max: 3,
-    speed: 400,
-    glare: true,
-    'max-glare': 0.1,
-});
