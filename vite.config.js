2ace2135e0462cc1acda5b9dca1af38aa241c4d8
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-04-05 21:15:06 +0200

    v.1.19. Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  base: '/yacht-adventures/',
   root: 'src',
   build: {
     rollupOptions: {
       input: glob.sync('./src/*.html'),
     },
     outDir: '../dist',
+    assetsInlineLimit: 0,
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
